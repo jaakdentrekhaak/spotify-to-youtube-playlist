7992ab7a0774af3d73ceec16975707ff0cff5142
Jens Putzeys <jens.putzeys@student.kuleuven.be>
2021-09-09 15:48:23 +0200

    implement function for getting Spotify API access token

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
 # About
 This program converts a Spotify playlist into a YouTube playlist.
-Note: the program only works if the selected playlist is public (so you can reach it without logging in)
+Note: this program is not meant to be used by other people than me, since you need to have a client_id and client_secret (unless you have your own client_id and client_secret)
 
-# Authorization
-Get Spotify API token from https://developer.spotify.com/console/get-playlist/ by clicking "GET TOKEN" and then choosing playlist-read-private and logging in. Add this token to the `config.json` file as follows:
+# Development
+The client_id and client_secret for this project can be found on my Spotify developers dashboard: https://developer.spotify.com/dashboard/applications. The client_id and client_secret need to be added to a `config.json` file in the root folder that looks like this:
 ```json
 {
     "spotify": {
-        "token": "{YOUR_TOKEN}"
+        "client_id": "",
+        "client_secret": ""
     }
 }
 ```
--- spotify-to-youtube-playlist/spotify.py
+++ spotify-to-youtube-playlist/spotify.py
@@ -1,5 +1,36 @@
 import requests
 import json
+import base64
+
+
+def get_access_token():
+    """Retrieve the access token needed for communicating with the Spotify API.
+    The client-id and the client-secret from the config.json file are used for this (also see README).
+
+    Returns:
+        string: Spotify API access token
+    """
+    # https://developer.spotify.com/documentation/general/guides/authorization-guide/#client-credentials-flow
+    with open('config.json', 'r') as config_file:
+        conf = json.load(config_file)
+        client_id = conf['spotify']['client_id']
+        client_secret = conf['spotify']['client_secret']
+    auth = f'{client_id}:{client_secret}'
+    auth_bytes = auth.encode('ascii')
+    auth_base64_bytes = base64.b64encode(auth_bytes)
+    auth_base64 = auth_base64_bytes.decode('ascii')
+
+    headers = {
+        'Authorization': f'Basic {auth_base64}',
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+
+    response = requests.post(
+        'https://accounts.spotify.com/api/token',
+        data="grant_type=client_credentials",
+        headers=headers
+    )
+    return json.loads(response.content)['access_token']
 
 
 def get_track_names_from_playlist(playlist_id: str):
@@ -13,9 +44,7 @@ def get_track_names_from_playlist(playlist_id: str):
     """
     playlist_url = f'https://api.spotify.com/v1/playlists/{playlist_id}'
 
-    with open('config.json', 'r') as file:
-        conf = json.load(file)
-        token = conf['spotify']['token']
+    token = get_access_token()
 
     headers = {
         'Accept': 'application/json',
